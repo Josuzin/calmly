488093fd22a7bd96532f34310b8ff6924061517f
Josué <josue.jackson15@outlook.pt>
2024-05-23 12:22:36 +0100

    Minor Changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,7 @@ import HomePage from "../../pages/firstpage/HomePage";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <HomePage/>
-=======
-    <SideBar/>
->>>>>>> 80c79bad8e4d8c2f6c99245ee4f27b7d934f038a
     
   );
 }
