ae479425b051efd63012571a50f36f7d8acecb82
MadalenaAntunes <madalena.nfantunes@gmail.com>
2024-05-23 12:29:15 +0100

    same as before mas sem bug :)

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
 import Image from "next/image";
 import LoginForm from "./loginform/LoginForm";
 import SideBar from "../../components/SideBar";
-<<<<<<< HEAD
 import HomePage from "../../components/HomePage";
 import MeditationPage from "../../components/MeditationPage";
 
 export default function Home() {
   return (
-    <MeditationPage/>
-=======
-import HomePage from "../../pages/firstpage/HomePage";
-
-
-export default function Home() {
-  return (
     <HomePage/>
->>>>>>> 488093fd22a7bd96532f34310b8ff6924061517f
-    
   );
 }
