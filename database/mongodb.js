c523b5f50ce18203f8c251fc8a9afb674ca5439a
Josué <josue.jackson15@outlook.pt>
2024-05-29 16:49:49 +0100

    Musica nova pro dive in

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,116 +1,52 @@
-// import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-<<<<<<< HEAD
 export const connectMongoDB = async () => {
+    let client;
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        client = await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB connected");
     } catch (error) {
         console.log("MongoDB error", error);
-        
-=======
-// export const connectMongoDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL);
-//         console.log("MongoDB connected");
-//     } catch (error) {
-//         console.log("MongoDB error", error);
-
-//     }
-// }
-
-//  import mongoose from 'mongoose';
-
-//  const MONGODB_URI = process.env.MONGODB_URI;
-
-//  if (!MONGODB_URI) {
-//    throw new Error(
-//      'Por favor, defina a variável de ambiente MONGODB_URI no seu arquivo .env.local'
-//    );
-//  }
-
-//  let cached = global.mongoose;
-
-//  if (!cached) {
-//    cached = global.mongoose = { conn: null, promise: null };
-//  }
-
-//  async function connectToDatabase() {
-//    if (cached.conn) {
-//      return cached.conn;
-//    }
-
-//    if (!cached.promise) {
-//      const opts = {
-//        useNewUrlParser: true,
-//        useUnifiedTopology: true,
-//      };
-
-//      cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-//        return mongoose;
-//      });
-//    }
-//    cached.conn = await cached.promise;
-//    return cached.conn;
-//  }
-
-//  export default connectToDatabase;
-
-import { MongoClient } from "mongodb";
-const URL = process.env.MONGODB_URI ?? "mongodb://localhost:27017";
-
-let client;
-async function connectToMongo() {
-  try {
-    if (!client) {
-      client = await MongoClient.connect(URL);
->>>>>>> df4b516627f543f9f9bbbd47f3ad1df5ed24a06a
     }
     return client;
-  } catch (err) {
-    console.log(err);
-  }
+};
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    throw new Error(
+        'Por favor, defina a variável de ambiente MONGODB_URI no seu arquivo .env.local'
+    );
 }
 
-<<<<<<< HEAD
+let cached = global.mongoose;
 
- const MONGODB_URI = process.env.MONGODB_URI;
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
 
- if (!MONGODB_URI) {
-   throw new Error(
-     'Por favor, defina a variável de ambiente MONGODB_URI no seu arquivo .env.local'
-   );
- }
+async function connectToDatabase() {
+    if (cached.conn) {
+        return cached.conn;
+    }
 
- let cached = global.mongoose;
+    if (!cached.promise) {
+        const opts = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
 
- if (!cached) {
-   cached = global.mongoose = { conn: null, promise: null };
- }
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
 
- async function connectToDatabase() {
-   if (cached.conn) {
-     return cached.conn;
-   }
+export default connectToDatabase;
 
-   if (!cached.promise) {
-     const opts = {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-     };
-
-     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-       return mongoose;
-     });
-   }
-   cached.conn = await cached.promise;
-   return cached.conn;
- }
-
- export default connectToDatabase;
-=======
 export async function getMongoCollection(collectionName) {
-  const client = await connectToMongo();
-  return client.db("CalmLy").collection(collectionName);
+    const client = await connectMongoDB();
+    return client.db("CalmLy").collection(collectionName);
 }
->>>>>>> df4b516627f543f9f9bbbd47f3ad1df5ed24a06a
